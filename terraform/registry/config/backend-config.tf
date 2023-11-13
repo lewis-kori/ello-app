71916ec4f7593bb18c228c51dad5ec2d792d7bd8
lewis-kori <lewiskosh@gmail.com>
2023-11-13 19:40:00 +0300

    update terraform gitignore configuration

---
+++ terraform/registry/config/backend-config.tf
@@ -0,0 +1,5 @@
+bucket                      = "ello-terraform-registry"
+key                         = "<replace-with-your-key.tfstate>"
+region                      = "us-east-1"
+encrypt                     = true
+dynamodb_table              = "ello-app-db"
---
+++ terraform/registry/vars.tfvars
@@ -0,0 +1,2 @@
+registry_name = "ello-app"
+aws_region = "us-east-1"
