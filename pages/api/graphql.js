ff1df67b00ea4f7ff8e0cf0823b6d4a660dce8c9
lewis-kori <lewiskosh@gmail.com>
2023-11-15 11:17:04 +0300

    fix barbel compile time error

--- pages/api/graphql.js
+++ pages/api/graphql.js
@@ -2,6 +2,7 @@ import { ApolloServer } from '@apollo/server';
 import { startServerAndCreateNextHandler } from '@as-integrations/next';
 import resolvers from './resolvers';
 import typeDefs from './schemas';
+import React from 'react';
 
 const apolloServer = new ApolloServer({
   typeDefs,
