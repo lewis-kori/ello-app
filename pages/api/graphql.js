14d996a127e9db1453ce72e20f6e66299efe2a0b
Lewis Kori <39276332+lewis-kori@users.noreply.github.com>
2023-11-15 10:29:24 +0300

    Unit Test config (#11)
    * Add ssl cert arn to terraform config
    
    * configure basic unit tests for github actions
    - modify books constants

--- pages/api/graphql.js
+++ pages/api/graphql.js
@@ -1,71 +1,7 @@
 import { ApolloServer } from '@apollo/server';
 import { startServerAndCreateNextHandler } from '@as-integrations/next';
-import { gql } from 'graphql-tag';
-const fs = require('fs');
-const path = require('path');
-
-import { bookTokenizer } from '@utils/stringSanitizers';
-const booksDirectoryPath = './resources';
-
-const books = [];
-
-fs.readdir(booksDirectoryPath, (err, files) => {
-  if (err) {
-    console.error('Error reading directory:', err);
-    return;
-  }
-
-  files.forEach((file) => {
-    const filePath = path.join(booksDirectoryPath, file);
-    if (path.extname(file) === '.json') {
-      try {
-        const fileContent = fs.readFileSync(filePath, 'utf-8');
-        const jsonData = JSON.parse(fileContent);
-        books.push(jsonData);
-      } catch (error) {
-        console.error('Error reading JSON file:', filePath, error);
-      }
-    }
-  });
-
-});
-
-const typeDefs = gql`
-  type Query {
-    book(title: String): Book
-  }
-
-  type Book {
-    title: String
-    author: String
-    pages: [Page!]
-  }
-
-  type Page {
-    pageIndex: Int
-    content: [Content!]
-  }
-
-  type Content {
-    index: Int
-    token: String
-    content: String
-    isTappable: Boolean
-  }
-`;
-
-const resolvers = {
-  Query: {
-    book(parent, args, contextValue, info) {
-      const book = books.find(
-        (book) =>
-          book.title.toLowerCase().trim() === args.title.toLowerCase().trim()
-      );
-      let tokenizedBook = bookTokenizer(book)
-      return tokenizedBook
-    },
-  },
-};
+import resolvers from './resolvers';
+import typeDefs from './schemas';
 
 const apolloServer = new ApolloServer({
   typeDefs,
---
+++ pages/api/schemas/index.js
@@ -0,0 +1,27 @@
+import { gql } from 'graphql-tag';
+
+const typeDefs = gql`
+  type Query {
+    book(title: String): Book
+  }
+
+  type Book {
+    title: String
+    author: String
+    pages: [Page!]
+  }
+
+  type Page {
+    pageIndex: Int
+    content: [Content!]
+  }
+
+  type Content {
+    index: Int
+    token: String
+    content: String
+    isTappable: Boolean
+  }
+`;
+
+export default typeDefs;
